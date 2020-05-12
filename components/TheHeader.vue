56199c118b753b9dc1964a5a7f4c367f928dc307
petepall <peter@ppallen.be>
2020-05-12 20:07:22 +0200

    Refactored header into own component
    Signed-off-by: petepall <peter@ppallen.be>

---
+++ components/TheHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-navigation-drawer v-model="sideNav" temporary app>
+      <v-list>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark class="primary">
+      <v-app-bar-nav-icon
+        class="hidden-sm-and-up"
+        @click.stop="sideNav = !sideNav"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <nuxt-link to="/" tag="span" style="cursor: pointer"
+          >DevMeetup</nuxt-link
+        >
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn v-for="item in menuItems" :key="item.title" text :to="item.link">
+          <v-icon left dark>{{ item.icon }}</v-icon
+          >{{ item.title }}</v-btn
+        >
+      </v-toolbar-items>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+import {
+  mdiAccountSupervisor,
+  mdiAccount,
+  mdiFace,
+  mdiLockOpen,
+  mdiMapMarker
+} from "@mdi/js";
+
+export default {
+  data: () => ({
+    sideNav: false,
+    menuItems: [
+      { icon: mdiAccountSupervisor, title: "View Meetups", link: "/meetups" },
+      { icon: mdiMapMarker, title: "Organize Meetup", link: "/createMeetup" },
+      { icon: mdiFace, title: "Profile", link: "/profile" },
+      { icon: mdiAccount, title: "Sign-up", link: "/signUp" },
+      { icon: mdiLockOpen, title: "Sign-in", link: "/signIn" }
+    ]
+  })
+};
+</script>
+
+<style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,6 @@
 <template>
   <v-app light>
-    <v-navigation-drawer v-model="sideNav" temporary app>
-      <v-list>
-        <v-list-item
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.link"
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>{{ item.title }}</v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app dark class="primary">
-      <v-app-bar-nav-icon
-        class="hidden-sm-and-up"
-        @click.stop="sideNav = !sideNav"
-      ></v-app-bar-nav-icon>
-      <v-toolbar-title>
-        <nuxt-link to="/" tag="span" style="cursor: pointer"
-          >DevMeetup</nuxt-link
-        >
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn v-for="item in menuItems" :key="item.title" text :to="item.link">
-          <v-icon left dark>{{ item.icon }}</v-icon
-          >{{ item.title }}</v-btn
-        >
-      </v-toolbar-items>
-    </v-app-bar>
-
+    <TheHeader />
     <v-content app>
       <nuxt />
     </v-content>
@@ -43,24 +10,28 @@
 </template>
 
 <script>
-import {
-  mdiAccountSupervisor,
-  mdiAccount,
-  mdiFace,
-  mdiLockOpen,
-  mdiMapMarker
-} from "@mdi/js";
+import TheHeader from "../components/TheHeader";
+// import {
+//   mdiAccountSupervisor,
+//   mdiAccount,
+//   mdiFace,
+//   mdiLockOpen,
+//   mdiMapMarker
+// } from "@mdi/js";
 
 export default {
-  data: () => ({
-    sideNav: false,
-    menuItems: [
-      { icon: mdiAccountSupervisor, title: "View Meetups", link: "/meetups" },
-      { icon: mdiMapMarker, title: "Organize Meetup", link: "/createMeetup" },
-      { icon: mdiFace, title: "Profile", link: "/profile" },
-      { icon: mdiAccount, title: "Sign-up", link: "/signUp" },
-      { icon: mdiLockOpen, title: "Sign-in", link: "/signIn" }
-    ]
-  })
+  components: {
+    TheHeader
+  }
+  // data: () => ({
+  //   sideNav: false,
+  //   menuItems: [
+  //     { icon: mdiAccountSupervisor, title: "View Meetups", link: "/meetups" },
+  //     { icon: mdiMapMarker, title: "Organize Meetup", link: "/createMeetup" },
+  //     { icon: mdiFace, title: "Profile", link: "/profile" },
+  //     { icon: mdiAccount, title: "Sign-up", link: "/signUp" },
+  //     { icon: mdiLockOpen, title: "Sign-in", link: "/signIn" }
+  //   ]
+  // })
 };
 </script>
