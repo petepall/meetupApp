f41a4293ef79fd85e287d6f7360f0c2b76a47c94
petepall <peter@ppallen.be>
2020-05-23 20:39:11 +0200

    create Alert component
    Signed-off-by: petepall <peter@ppallen.be>

---
+++ components/shared/Alert.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-alert :value="true" type="error" dismissible @input="onClose">
+    Message: {{ text }}
+  </v-alert>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/require-prop-types
+  props: ["text"],
+  methods: {
+    onClose() {
+      this.$emit("dismissed");
+    }
+  }
+};
+</script>
+
+<style scoped></style>
