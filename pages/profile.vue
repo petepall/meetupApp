2e79932fb685be4fcad351bd9942f73c42948bc2
petepall <peter@ppallen.be>
2020-05-24 18:16:16 +0200

    Setup basic button for logging out a user
    Signed-off-by: petepall <peter@ppallen.be>

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,11 +1,71 @@
 <template>
-  <div>
-    <p>The User page</p>
-  </div>
+  <v-container>
+    <v-row v-if="error">
+      <v-col xs12 sm6 offset-sm3>
+        <app-alert :text="error.message" @dismissed="onDismissed"></app-alert>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="onSignout">
+                <v-row>
+                  <v-col>
+                    <v-btn type="submit" :disabled="loading" :loading="loading">
+                      Sign out
+                      <template v-slot:loader>
+                        <span class="custom-loader">
+                          <v-icon light>$loading</v-icon>
+                        </span>
+                      </template>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({}),
+
+  computed: {
+    user() {
+      return this.$store.getters.user;
+    },
+    error() {
+      return this.$store.getters.error;
+    },
+    loading() {
+      return this.$store.getters.loading;
+    }
+  },
+
+  watch: {
+    user(val) {
+      if (val !== null && val !== undefined) {
+        this.$router.push("/");
+      }
+    }
+  },
+
+  methods: {
+    onSignout() {
+      this.$store.dispatch("signUserOut", {});
+    },
+
+    onDismissed() {
+      this.$store.commit("clearError");
+    }
+  }
+};
 </script>
 
 <style></style>
